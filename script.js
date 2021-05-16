4ddf3ceb96af1ac4342a9e3aae51831bb89dccc1
Hetvee Parikh <hetveeparikh9@gmail.com>
2021-05-16 16:02:56 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(principal<=0)
+    {
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = (principal * years * rate /100).toFixed(2);
+    var year = new Date().getFullYear()+parseInt(years);
+    var amount=parseFloat(interest)+parseFloat(principal);
+    //var val=document.getElementById("result");
+    //val.innerHTML="If you deposit "+"<mark>"+principal+"</mark>"+",<br>at an interest rate of "+"<mark>"+rate+"</mark>"+"%<br>You will receive an amount of "+"<mark>"+interest+"</mark>"+",<br>in the year "+"<mark>"+year+"</mark>"+"<br>";
+    document.getElementById("int_result").innerHTML="Interest Amount: "+"<b>"+ interest +"</b>";
+    document.getElementById("result").innerHTML="If you deposit <b>"+principal+"</b>,<br>at an interest rate of <b>"+rate+"</b>%<br>You will receive an amount of <b>"+interest+"</b>,<br>in the year <b>"+year+"</b><br>";
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
